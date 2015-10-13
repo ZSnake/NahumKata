56067b73d08f18803753ce0e5fd950ebd41c3524
Rene Rosa <realpasro09@hotmail.com>
2015-10-13 15:06:45 -0600

    adding when adding an empty string spec

---
+++ kata.specs/when_adding_an_empty_string.cs
@@ -0,0 +1,24 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs
+{
+    public class when_adding_an_empty_string
+    {
+        Establish context = () =>
+        {
+            _stringCalculator = new StringCalculator();
+        };
+        private Because of = () =>
+        {
+            _result = _stringCalculator.Add("");
+        };
+
+        private It should_return_0 = () =>
+        {
+            _result.ShouldBeLike(0);
+        };
+
+        private static StringCalculator _stringCalculator;
+        private static int _result;
+    }
+}
---
+++ kata/StringCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace kata
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
